6d8341ecd5d77b1b5e28ba63b2bb0e9cd2256e37
luu-alex <98a.lexluu@gmail.com>
2024-11-13 12:37:46 +0700

    add resources

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,5 @@ import vercel from 'vite-plugin-vercel';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), vercel()],
-	server: {
-		proxy: {
-			'/api': {
-				target: 'http://159.223.40.127:5001', // Your backend server URL
-				changeOrigin: true,
-				rewrite: path => path.replace(/^\/api/, '/api'),
-			},
-		},
-	},
+	
 });
